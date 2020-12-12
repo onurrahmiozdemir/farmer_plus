13e42163facce2b3d7a8ec117e13157be597dcb7
XqurieM <75906324+XqurieM@users.noreply.github.com>
2020-12-12 22:09:30 +0300

    dashboadr styles dosyası eklendi (#1)

--- src/Screen/Dashboard/index.tsx
+++ src/Screen/Dashboard/index.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 import {SafeAreaView, FlatList, Text} from 'react-native';
 import Header from '../../component/Header';
-
+import Styles from './styles';
 const Dashboard = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={Styles.container}>
       <Header goBack={false} goBackIconColor={'#fff'} title={'Dashboard'} />
 
       <Text>Dashboard</Text>
